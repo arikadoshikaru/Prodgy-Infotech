c596a029bc9b94b1e3d9011e9768e43e5944c051
Saurav Kumar <saurav34developer@outlook.in>
2025-08-05 13:09:37 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Updated URL for a reliable Titanic dataset
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+# Create a figure with two subplots
+fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+# --- Plot 1: Bar Chart for Gender Distribution ---
+sns.countplot(x='Sex', data=df, palette='viridis', ax=axes[0])
+axes[0].set_title('Distribution of Genders on the Titanic')
+axes[0].set_xlabel('Gender')
+axes[0].set_ylabel('Number of Passengers')
+
+# --- Plot 2: Histogram for Age Distribution ---
+sns.histplot(data=df, x='Age', bins=20, kde=True, color='skyblue', ax=axes[1])
+axes[1].set_title('Distribution of Passenger Ages on the Titanic')
+axes[1].set_xlabel('Age')
+axes[1].set_ylabel('Frequency')
+
+# Adjust layout to prevent titles and labels from overlapping
+plt.tight_layout()
+plt.show()
